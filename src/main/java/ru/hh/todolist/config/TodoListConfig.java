c3f2c4ecad4251a0167e911e05d2207cbfe91e8b
bannovdaniil <baddan@mail.ru>
2023-02-08 14:46:17 +0300

    !7. убрал избыточный код

--- src/main/java/ru/hh/todolist/config/TodoListConfig.java
+++ src/main/java/ru/hh/todolist/config/TodoListConfig.java
@@ -3,22 +3,10 @@ package ru.hh.todolist.config;
 import org.glassfish.jersey.server.ResourceConfig;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
-import org.springframework.context.annotation.Import;
 import ru.hh.todolist.resource.TodoListResource;
-import ru.hh.todolist.service.TodoListService;
-import ru.hh.todolist.service.impl.TodoListServiceImpl;
 
 @Configuration
-@Import({
-    TodoListServiceImpl.class,
-    TodoListResource.class
-})
 public class TodoListConfig {
-  @Bean
-  public TodoListResource todoListResource(TodoListService todoListService) {
-    return new TodoListResource(todoListService);
-  }
-
   @Bean
   public ResourceConfig jerseyConfig() {
     ResourceConfig resourceConfig = new ResourceConfig();
